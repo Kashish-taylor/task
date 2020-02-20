65a67af4dad9397544d520671f66ed8f961a1a88
kashish tailor <kahish.taylor@gmail.com>
2020-02-20 12:46:50 +0530

    init

---
+++ K.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class K
+{
+public static void main(String ...ar)
+{
+ Scanner sc=new Scanner(System.in);
+int  i=sc.nextInt();
+if(i%9==0)
+System.out.println("number is  divisible by 9");
+else
+System.out.println("number is not divisibale by 9");
+}
+}
